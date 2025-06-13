514256a7660a4852aed4125dc007cccc0912ef63
GaukarbekAkmaral <akmaralgaukarbek2006@gmail.com>
2025-06-13 16:28:11 +0500

    add

---
+++ Lab5/row.py
@@ -0,0 +1,34 @@
+#1️ match_a_followed_by_b(string)
+print(match_a_followed_by_b("abbb"))  
+print(match_a_followed_by_b("aaa"))   
+print(match_a_followed_by_b("b"))     
+
+#2 match_a_followed_by_2_to_3_b(string)
+print(match_a_followed_by_2_to_3_b("abb"))   
+print(match_a_followed_by_2_to_3_b("abbbb")) 
+
+#3️ find_lowercase_with_underscore(string)
+print(find_lowercase_with_underscore("this_is_a_test example_text"))  
+
+#4️ find_upper_followed_by_lower(string)
+print(find_upper_followed_by_lower("Hello World Test String"))  
+
+#5 match_a_followed_by_anything_ending_b(string)
+print(match_a_followed_by_anything_ending_b("axxxb"))  
+print(match_a_followed_by_anything_ending_b("ab"))     
+print(match_a_followed_by_anything_ending_b("acb"))    
+
+#6 replace_spaces_commas_dots(string)
+print(replace_spaces_commas_dots("Hello, world. How are you?"))  
+
+#7 snake_to_camel(snake_str)
+print(snake_to_camel("this_is_a_test"))  
+
+#8 split_at_uppercase(string)
+print(split_at_uppercase("SplitAtUppercase"))  
+
+#9️ insert_spaces_between_capitals(string)
+print(insert_spaces_between_capitals("InsertSpacesBetweenCapitals"))  
+
+#10 camel_to_snake(camel_str)
+print(camel_to_snake("CamelCaseString"))  
